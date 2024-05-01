bd394610ffa5f8f162e61bac50300a82ca36bd91
MareezAdell <147976343+MareezAdell@users.noreply.github.com>
2024-05-01 17:52:40 +0300

    Update setup.py

--- DataPrepKit/setup.py
+++ DataPrepKit/setup.py
@@ -5,7 +5,7 @@ setup(
     version = "1.0.0",
     author = " Mareez Adel Guirguis",
     author_email = "mareez.adel00@gmail.com",
-    description = "AThe DataPrepKit project is a comprehensive Python toolkit designed to streamline common data preparation tasks.",
+    description = "The DataPrepKit project is a comprehensive Python toolkit designed to streamline common data preparation tasks.",
     packages = find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
