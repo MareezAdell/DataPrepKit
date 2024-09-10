a96f292d5f9142763f5aa451fd701026abab4989
MareezAdell <147976343+MareezAdell@users.noreply.github.com>
2024-09-10 19:12:22 +0300

    Delete Own PyCaret directory

--- Own PyCaret/ML_Package/__init__.py
+++
@@ -1,3 +0,0 @@
-from .data_loading import DataLoader
-from .eda import summarize_data, handle_missing_values, encode_categorical_variables, scale_numerical_features
-from .model_training_and_selection import ModelTrainer
